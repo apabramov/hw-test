3900328af090065fd5fb8965fea6035ba48491e9
APAbramov <APAbramov@rolf.ru>
2022-10-04 16:50:13 +0300

    refactoring

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,42 +11,45 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
 	var b strings.Builder
-	var i int = 0
-	var r []rune = []rune(s)
+	var i int
 	var prevLetter bool
+	var r = []rune(s)
 
-	if s == "" {
+	switch s {
+	case "":
 		return "", nil
-	}
-	for {
-		if unicode.IsLetter(r[i]) {
-			b.WriteRune(r[i])
-			i++
-			prevLetter = true
-		} else if unicode.IsDigit(r[i]) && i != 0 && prevLetter {
-			n, _ := strconv.Atoi(string(r[i]))
-			str := b.String()
-			if n > 0 {
-				b.WriteString(strings.Repeat(str[len(str)-1:], n-1))
-			} else if n == 0 {
-				s := b.String()
-				s = s[:len(s)-1]
-				b.Reset()
-				b.WriteString(s)
+	default:
+		for {
+			if unicode.IsLetter(r[i]) {
+				b.WriteRune(r[i])
+				i++
+				prevLetter = true
+			} else if unicode.IsDigit(r[i]) && i != 0 && prevLetter {
+				n, _ := strconv.Atoi(string(r[i]))
+				str := b.String()
+				last := str[len(str)-1:]
+				if n > 0 {
+					b.WriteString(strings.Repeat(last, n-1))
+				} else if n == 0 {
+					s := b.String()
+					s = s[:len(s)-1]
+					b.Reset()
+					b.WriteString(s)
+				}
+				i++
+				prevLetter = false
+			} else if r[i] == '\\' {
+				i++
+				b.WriteRune(r[i])
+				i++
+				prevLetter = true
+			} else {
+				return "", ErrInvalidString
 			}
-			i++
-			prevLetter = false
-		} else if r[i] == '\\' {
-			i++
-			b.WriteRune(r[i])
-			i++
-			prevLetter = true
-		} else {
-			return "", ErrInvalidString
+			if i == len(s) {
+				break
+			}
 		}
-		if i == len(s) {
-			break
-		}
+		return b.String(), nil
 	}
-	return b.String(), nil
 }
