07efda96a484f50efdb7694d7eea2414963df934
APAbramov <APAbramov@rolf.ru>
2022-11-01 13:38:59 +0300

    -

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,85 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"fmt"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+type Worker struct {
+	in     chan Task
+	outErr chan error
+	done   chan struct{}
+	wg     *sync.WaitGroup
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	w := Worker{
+		in:     make(chan Task),
+		outErr: make(chan error),
+		done:   make(chan struct{}),
+		wg:     &sync.WaitGroup{},
+	}
+
+	go func(w Worker) {
+		defer close(w.in)
+
+		for i := range tasks {
+			if IsClosed(w.done) {
+				return
+			}
+			w.in <- tasks[i]
+		}
+	}(w)
+
+	go func(w Worker) {
+		for i := 0; i < n; i++ {
+			w.wg.Add(1)
+			go do(w)
+		}
+		w.wg.Wait()
+		close(w.outErr)
+	}(w)
+
+	cnt := 0
+	err := false
+	for res := range w.outErr {
+		cnt++
+		fmt.Println(res)
+		if cnt == m {
+			close(w.done)
+			err = true
+		}
+	}
+
+	if err {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
+}
+
+func IsClosed(ch <-chan struct{}) bool {
+	select {
+	case <-ch:
+		return true
+	default:
+	}
+	return false
+}
+
+func do(w Worker) {
+	defer w.wg.Done()
+
+	for t := range w.in {
+		if IsClosed(w.done) {
+			return
+		}
+		if err := t(); err != nil {
+			w.outErr <- err
+		}
+	}
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,30 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("tasks without errors without sleep", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+		var sumTime time.Duration
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+			sumTime += taskSleep
+
+			tasks = append(tasks, func() error {
+				time.Sleep(taskSleep)
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 4
+		maxErrorsCount := 1
+
+		require.Eventually(t, func() bool {
+			err := Run(tasks, workersCount, maxErrorsCount)
+			return err == nil
+		}, sumTime/3, time.Millisecond, "extra tasks were started")
+	})
 }
