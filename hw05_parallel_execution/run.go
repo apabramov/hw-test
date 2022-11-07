ab682f58d70c4e194c48a601548a9f8d0231ead0
APAbramov <APAbramov@rolf.ru>
2022-11-07 17:44:53 +0300

    HW05

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,85 +2,51 @@ package hw05parallelexecution
 
 import (
 	"errors"
-	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-type Worker struct {
-	in     chan Task
-	outErr chan error
-	done   chan struct{}
-	wg     *sync.WaitGroup
-}
-
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	w := Worker{
-		in:     make(chan Task),
-		outErr: make(chan error),
-		done:   make(chan struct{}),
-		wg:     &sync.WaitGroup{},
-	}
-
-	go func(w Worker) {
-		defer close(w.in)
-
-		for i := range tasks {
-			if IsClosed(w.done) {
-				return
-			}
-			w.in <- tasks[i]
-		}
-	}(w)
-
-	go func(w Worker) {
-		for i := 0; i < n; i++ {
-			w.wg.Add(1)
-			go do(w)
-		}
-		w.wg.Wait()
-		close(w.outErr)
-	}(w)
-
-	cnt := 0
-	err := false
-	for res := range w.outErr {
-		cnt++
-		fmt.Println(res)
-		if cnt == m {
-			close(w.done)
-			err = true
-		}
-	}
-
-	if err {
+	if n <= 0 || m <= 0 {
 		return ErrErrorsLimitExceeded
 	}
+
+	var cntErr int32
+	taskIn := make(chan Task)
+	wg := sync.WaitGroup{}
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go do(&wg, taskIn, &cntErr)
+	}
+
+	for _, t := range tasks {
+		if atomic.LoadInt32(&cntErr) >= int32(m) {
+			break
+		}
+		taskIn <- t
+	}
+
+	close(taskIn)
+	wg.Wait()
+
+	if cntErr >= int32(m) {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
 
-func IsClosed(ch <-chan struct{}) bool {
-	select {
-	case <-ch:
-		return true
-	default:
-	}
-	return false
-}
+func do(wg *sync.WaitGroup, taskIn chan Task, cnt *int32) {
+	defer wg.Done()
 
-func do(w Worker) {
-	defer w.wg.Done()
-
-	for t := range w.in {
-		if IsClosed(w.done) {
-			return
-		}
+	for t := range taskIn {
 		if err := t(); err != nil {
-			w.outErr <- err
+			atomic.AddInt32(cnt, 1)
 		}
 	}
 }
