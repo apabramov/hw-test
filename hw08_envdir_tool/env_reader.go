e5477d5b7eec71a83cd1067df3bd3f3c61477d8d
APAbramov <APAbramov@rolf.ru>
2022-12-13 16:12:27 +0300

    HW08

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"errors"
 	"fmt"
 	"os"
@@ -30,31 +29,28 @@ func ReadDir(dir string) (Environment, error) {
 		return nil, err
 	}
 	for _, fi := range files {
-		func() {
-			f, err := os.Open(path.Join(dir, fi.Name()))
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			defer f.Close()
+		f, err := os.Open(path.Join(dir, fi.Name()))
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+		scanner := bufio.NewScanner(f)
+		scanner.Split(bufio.ScanLines)
+		scanner.Scan()
+		str := scanner.Text()
 
-			scanner := bufio.NewScanner(f)
-			scanner.Split(bufio.ScanLines)
-			scanner.Scan()
-			str := scanner.Text()
+		s := strings.ReplaceAll(str, "\u0000", "\n")
 
-			s := bytes.ReplaceAll([]byte(str), []byte{0}, []byte{10})
-
-			val := strings.TrimRight(string(s), "\r")
-			val = strings.TrimRight(val, "\t")
-			val = strings.TrimRight(val, " ")
-			val = strings.TrimRight(val, "\t")
-			if str == "" {
-				e[fi.Name()] = EnvValue{Value: "", NeedRemove: true}
-			} else {
-				e[fi.Name()] = EnvValue{Value: val, NeedRemove: false}
-			}
-		}()
+		val := strings.TrimRight(s, "\r")
+		val = strings.TrimRight(val, "\t")
+		val = strings.TrimRight(val, " ")
+		val = strings.TrimRight(val, "\t")
+		if str == "" {
+			e[fi.Name()] = EnvValue{Value: "", NeedRemove: true}
+		} else {
+			e[fi.Name()] = EnvValue{Value: val, NeedRemove: false}
+		}
+		f.Close()
 	}
 	return e, nil
 }
