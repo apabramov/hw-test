cd3ce66b32770eb532c0db107fc2afa3b347a530
APAbramov <APAbramov@rolf.ru>
2022-12-28 18:25:40 +0300

    add test

--- hw11_telnet_client/telnet_test.go
+++ hw11_telnet_client/telnet_test.go
@@ -62,4 +62,47 @@ func TestTelnetClient(t *testing.T) {
 
 		wg.Wait()
 	})
+
+	t.Run("error connection", func(t *testing.T) {
+		client := NewTelnetClient("host", time.Second*1, nil, nil)
+		require.Error(t, client.Connect())
+	})
+
+	t.Run("server send nil", func(t *testing.T) {
+		l, err := net.Listen("tcp", "127.0.0.1:")
+		require.NoError(t, err)
+		defer func() { require.NoError(t, l.Close()) }()
+
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		go func() {
+			defer wg.Done()
+
+			in := &bytes.Buffer{}
+			out := &bytes.Buffer{}
+
+			client := NewTelnetClient(l.Addr().String(), time.Second*1, io.NopCloser(in), out)
+			require.NoError(t, client.Connect())
+			defer func() { require.NoError(t, client.Close()) }()
+
+			err = client.Receive()
+			require.NoError(t, err)
+		}()
+
+		go func() {
+			defer wg.Done()
+
+			conn, err := l.Accept()
+			require.NoError(t, err)
+			require.NotNil(t, conn)
+			defer func() { require.NoError(t, conn.Close()) }()
+
+			n, err := conn.Write(nil)
+			require.NoError(t, err)
+			require.Equal(t, 0, n)
+		}()
+
+		wg.Wait()
+	})
 }
