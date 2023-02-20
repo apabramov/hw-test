5c3a4bf45f1abd1c394c82da046f3cde7ec7c748
APAbramov <APAbramov@rolf.ru>
2023-02-20 11:33:39 +0300

    lint

--- hw12_13_14_15_calendar/generate.go
+++ hw12_13_14_15_calendar/generate.go
@@ -1,3 +1,4 @@
+// nolint
 package proto
 
 //go:generate 	protoc --go_out=internal/server/pb --go-grpc_out=internal/server/pb --grpc-gateway_out=internal/server/pb --grpc-gateway_opt generate_unbound_methods=true --openapiv2_out . api/EventService.proto
